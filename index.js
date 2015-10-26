ec6554204125eec41edb2f4887fc00d88f18a50a
XueMingxiang <xuemx@cleverloop.com>
2015-10-27 00:45:20 +0800

    first commit

---
+++ index.js
@@ -0,0 +1,36 @@
+'use strict';
+
+var gutil = require('gulp-util');
+var PluginError = gutil.PluginError;
+var through = require('through2');
+var Path = require('path');
+
+module.exports = function(opts) {
+	function createFile(context, file, locale) {
+		var extname = Path.extname(file.relative);
+		var basename = Path.basename(file.relative, extname);
+
+		var createdFile = file.clone({ contents: false });
+		createdFile.path = Path.join(file.base, basename + '.'+ locale + extname);
+		createdFile.contents = new Buffer(String(file.contents).replace(/\.html/g, '.' + locale + '.html'));
+		context.push(createdFile);
+	}
+
+	return through.obj(function(file, enc, cb) {
+		if (file.isNull()) { // ignore empty files
+			cb();
+			return;
+		}
+
+		if (file.isStream()) {
+			this.emit('error', new PluginError('gulp-html-locale',  'Streaming not supported'));
+			cb();
+			return;
+		}
+
+		createFile(context, file, 'en');
+		createFile(context, file, 'zh');
+
+		cb();
+	});
+};
